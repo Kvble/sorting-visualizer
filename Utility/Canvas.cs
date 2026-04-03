da79bd21abadb32eb1f0189af871a7ba4df2aace
Kevin Xavier Andrade <me@kevinxavierandrade.com>
2026-04-03 21:58:21 +0200

    fix: dispose GDI+ Brush and Pen objects to prevent resource leak

--- Utility/Canvas.cs
+++ Utility/Canvas.cs
@@ -8,12 +8,21 @@ namespace SortingVisualizer.Utility
         public Canvas() {}
         public void clearCanvas(int width, int height)
         {
-            Global.Graphics.FillRectangle(new SolidBrush(Color.White), 0, 0, width, height);
+            using (var brush = new SolidBrush(Color.White))
+            {
+                Global.Graphics.FillRectangle(brush, 0, 0, width, height);
+            }
         }
         public void drawRect(Color color, int xAxis, int yAxis)
         {
-            Global.Graphics.FillRectangle(new SolidBrush(color), xAxis, yAxis, Global.Width, Global.MaxHeight);
-            Global.Graphics.DrawRectangle(new Pen(Color.White, 1), xAxis, yAxis, Global.Width, Global.MaxHeight);
+            using (var brush = new SolidBrush(color))
+            {
+                Global.Graphics.FillRectangle(brush, xAxis, yAxis, Global.Width, Global.MaxHeight);
+            }
+            using (var pen = new Pen(Color.White, 1))
+            {
+                Global.Graphics.DrawRectangle(pen, xAxis, yAxis, Global.Width, Global.MaxHeight);
+            }
         }
     }
 }
